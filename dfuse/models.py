5f148233dcdeb78591049459bcd0137b3fe860a2
I'm The Cheese <ch33s3codes@gmail.com>
2019-08-27 17:54:58 +0300

    wip: start on returning types

--- dfuse/models.py
+++ dfuse/models.py
@@ -770,24 +770,27 @@ class TransactionLifecycle:
         'cancelation_irreversible': False
     }
     """
-    def __init__(self, id: str,
-                 transaction_status: str, transaction: Transaction,
-                 exectution_trace: TransactionTrace = None, exectution_block_header: BlockHeader = None,
-                 creation_tree: CreationTree = None, drtxops: List[DTrxOp] = None,
-                 ramops: List[RAMOp] = None, tableops: List[TableOp] = None,
-                 pub_keys: List[str] = None, created_by: ExtDTrxop = None, cancelled_by: ExtDTrxop = None, execution_irreversible: bool = None,
-                 creation_irreversible: bool = None, cancellation_irreversible: bool = None):
-        self.transaction_status = transaction_status
-        self.transaction = Transaction(transaction)
-        self.exectution_trace = exectution_trace
-        self.exectution_block_header = exectution_block_header
-        self.creation_tree = creation_tree
-        self.drtxops = drtxops
-        self.ramops = ramops
-        self.tableops = tableops
-        self.pub_keys = pub_keys
-        self.created_by = created_by
-        self.cancelled_by = cancelled_by
-        self.execution_irreversible = execution_irreversible
-        self.creation_irreversible = creation_irreversible
-        self.cancellation_irreversible = cancellation_irreversible
+    # def __init__(self, id: str,
+    #              transaction_status: str, transaction: Transaction,
+    #              exectution_trace: TransactionTrace = None, exectution_block_header: BlockHeader = None,
+    #              creation_tree: CreationTree = None, drtxops: List[DTrxOp] = None,
+    #              ramops: List[RAMOp] = None, tableops: List[TableOp] = None,
+    #              pub_keys: List[str] = None, created_by: ExtDTrxop = None, cancelled_by: ExtDTrxop = None, execution_irreversible: bool = None,
+    #              creation_irreversible: bool = None, cancellation_irreversible: bool = None):
+    #     self.transaction_status = transaction_status
+    #     self.transaction = Transaction(transaction)
+    #     self.exectution_trace = exectution_trace
+    #     self.exectution_block_header = exectution_block_header
+    #     self.creation_tree = creation_tree
+    #     self.drtxops = drtxops
+    #     self.ramops = ramops
+    #     self.tableops = tableops
+    #     self.pub_keys = pub_keys
+    #     self.created_by = created_by
+    #     self.cancelled_by = cancelled_by
+    #     self.execution_irreversible = execution_irreversible
+    #     self.creation_irreversible = creation_irreversible
+    #     self.cancellation_irreversible = cancellation_irreversible
+
+    def __init__(self, *args, **kwargs):
+        ...
