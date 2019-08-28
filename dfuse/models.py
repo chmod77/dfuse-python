3e58790c97e71501a91a424f7c26f58537637d3d
I'm The Cheese <ch33s3codes@gmail.com>
2019-08-28 17:56:00 +0300

    feat: add `type`s for `PermissionLink`, `KeyAccounts`, `Bin2JSON` and `ABI`

--- dfuse/models.py
+++ dfuse/models.py
@@ -589,9 +589,9 @@ class PublicKey:
     pass
 
 
-class AuthTokenResponse:
+class AuthTokenType:
     """
-        AuthTokenResponse
+        AuthTokenType
         Example payload:
 
         {
@@ -608,7 +608,7 @@ class AuthTokenResponse:
         self.expires_at = expires_at
 
 
-class BlockTimeStampResponse:
+class BlockTimeStampType:
     def __init__(self, *args, **kwargs):
         self.data = kwargs
 
@@ -648,7 +648,7 @@ class TableRows:
         self.rows = rows
 
 
-class TableSnapshotResponse:
+class TableSnapshotType:
     """
     TableSnapshotResponse
     Example table_snapshot response payload:
@@ -673,7 +673,7 @@ class TableSnapshotResponse:
 
     def __init__(self, type_: str, data: TableRows):
         self.type = type_
-        self.data = TableRows(data)
+        self.data = TableRows(**data)
 
 
 class Transaction:
@@ -681,24 +681,6 @@ class Transaction:
                  ref_block_prefix, max_net_usage_words=0,
                  max_cpu_usage_ms=0, delay_sec=0, context_free_actions=[],
                  actions=[], transaction_extensions=[], signatures=[], context_free_data=[]):
-        """
-        {
-            'expiration': '2019-04-16T14:36:11',
-            'ref_block_num': 65222,
-            'ref_block_prefix': 943310534,
-            'max_net_usage_words': 0,
-            'max_cpu_usage_ms': 0,
-            'delay_sec': 0,
-            'context_free_actions': [],
-            'actions': [{'account': 'maouehmaoueh',
-                'name': 'cfainline',
-                'authorization': [{'actor': 'maouehmaoueh', 'permission': 'active'}],
-                'hex_data': '03313233'}],
-            'transaction_extensions': [],
-            'signatures': ['SIG_K1_Jyv32XzrAGQepnk7p3YwXbRyNcp6Cztt8peR41GjfJ5hjDhjNfyf4ViubcShaDGd1BB9NEKGRrGjsQadzvwKrp7Wkjx9kh'],
-            'context_free_data': []
-            }
-        """
         self.expiration = expiration
         self.ref_block_num = ref_block_num
         self.ref_block_prefix = ref_block_prefix
@@ -714,66 +696,6 @@ class Transaction:
 
 class TransactionTrace:
     def __init__(self, id, block_num, block_time, producer_block_id, receipt, elapsed, net_usage, scheduled=False, action_traces=[], failed_dtrx_trace=None, **kwargs):
-        """
-        {
-            'id': '1d5f57e9392d045ef4d1d19e6976803f06741e11089855b94efcdb42a1a41253',
-            'block_num': 53280461,
-            'block_time': '2019-04-16T14:35:41.5',
-            'producer_block_id': '032cfecd63f2e42da2fb5b7f632acadbe5153756db615c5d28bbc99f9bd0976d',
-            'receipt': {
-                'status': 'executed',
-                'cpu_usage_us': 1191,
-                'net_usage_words': 12
-            },
-            'elapsed': 71934,
-            'net_usage': 96,
-            'scheduled': False,
-            'action_traces': [{'receipt': {'receiver': 'maouehmaoueh',
-                'act_digest': '5a2ffd1d0376049b5fcaa7d5f122723973c8fea40de9aba059bc439b4f77fd5e',
-                'global_sequence': '6249291689',
-                'auth_sequence': [['maouehmaoueh', 13]],
-                'recv_sequence': 5,
-                'code_sequence': 2,
-                'abi_sequence': 1},
-                'act': {'account': 'maouehmaoueh',
-                'name': 'cfainline',
-                'authorization': [{'actor': 'maouehmaoueh', 'permission': 'active'}],
-                'data': {'data': '123'},
-                'hex_data': '03313233'},
-                'context_free': False,
-                'elapsed': 71857,
-                'console': '',
-                'trx_id': '1d5f57e9392d045ef4d1d19e6976803f06741e11089855b94efcdb42a1a41253',
-                'block_num': 53280461,
-                'block_time': '2019-04-16T14:35:41.5',
-                'producer_block_id': '032cfecd63f2e42da2fb5b7f632acadbe5153756db615c5d28bbc99f9bd0976d',
-                'account_ram_deltas': None,
-                'except': None,
-                'inline_traces': [{'receipt': {'receiver': 'dfuseiohooks',
-                'act_digest': '469d68d4e68c0adc6fd302ca60b4d27256400965b90baf7a5736c9e0ed0b3d0f',
-                'global_sequence': '6249291690',
-                'auth_sequence': [],
-                'recv_sequence': 2,
-                'code_sequence': 0,
-                'abi_sequence': 1},
-                'act': {'account': 'dfuseiohooks',
-                'name': 'event',
-                'data': {'data': 'testing=123', 'key': ''},
-                'hex_data': '000b74657374696e673d313233'},
-                'context_free': True,
-                'elapsed': 11,
-                'console': '',
-                'trx_id': '1d5f57e9392d045ef4d1d19e6976803f06741e11089855b94efcdb42a1a41253',
-                'block_num': 53280461,
-                'block_time': '2019-04-16T14:35:41.5',
-                'producer_block_id': '032cfecd63f2e42da2fb5b7f632acadbe5153756db615c5d28bbc99f9bd0976d',
-                'account_ram_deltas': None,
-                'except': None,
-                'inline_traces': None}]}],
-            'failed_dtrx_trace': None,
-            'except': None
-            }
-        """
         self.id = id
         self.block_num = block_num
         self.block_time = block_time
@@ -787,103 +709,7 @@ class TransactionTrace:
         self.except_ = kwargs.get('except')
 
 
-
 class TransactionLifecycle:
-    """
-     {
-        'transaction_status': 'executed',
-        'id': '1d5f57e9392d045ef4d1d19e6976803f06741e11089855b94efcdb42a1a41253',
-        'transaction': {'expiration': '2019-04-16T14:36:11',
-        'ref_block_num': 65222,
-        'ref_block_prefix': 943310534,
-        'max_net_usage_words': 0,
-        'max_cpu_usage_ms': 0,
-        'delay_sec': 0,
-        'context_free_actions': [],
-        'actions': [{'account': 'maouehmaoueh',
-            'name': 'cfainline',
-            'authorization': [{'actor': 'maouehmaoueh', 'permission': 'active'}],
-            'hex_data': '03313233'}],
-        'transaction_extensions': [],
-        'signatures': ['SIG_K1_Jyv32XzrAGQepnk7p3YwXbRyNcp6Cztt8peR41GjfJ5hjDhjNfyf4ViubcShaDGd1BB9NEKGRrGjsQadzvwKrp7Wkjx9kh'],
-        'context_free_data': []},
-        'execution_trace': {'id': '1d5f57e9392d045ef4d1d19e6976803f06741e11089855b94efcdb42a1a41253',
-        'block_num': 53280461,
-        'block_time': '2019-04-16T14:35:41.5',
-        'producer_block_id': '032cfecd63f2e42da2fb5b7f632acadbe5153756db615c5d28bbc99f9bd0976d',
-        'receipt': {'status': 'executed',
-        'cpu_usage_us': 1191,
-        'net_usage_words': 12},
-        'elapsed': 71934,
-        'net_usage': 96,
-        'scheduled': False,
-        'action_traces': [{'receipt': {'receiver': 'maouehmaoueh',
-            'act_digest': '5a2ffd1d0376049b5fcaa7d5f122723973c8fea40de9aba059bc439b4f77fd5e',
-            'global_sequence': '6249291689',
-            'auth_sequence': [['maouehmaoueh', 13]],
-            'recv_sequence': 5,
-            'code_sequence': 2,
-            'abi_sequence': 1},
-            'act': {'account': 'maouehmaoueh',
-            'name': 'cfainline',
-            'authorization': [{'actor': 'maouehmaoueh', 'permission': 'active'}],
-            'data': {'data': '123'},
-            'hex_data': '03313233'},
-            'context_free': False,
-            'elapsed': 71857,
-            'console': '',
-            'trx_id': '1d5f57e9392d045ef4d1d19e6976803f06741e11089855b94efcdb42a1a41253',
-            'block_num': 53280461,
-            'block_time': '2019-04-16T14:35:41.5',
-            'producer_block_id': '032cfecd63f2e42da2fb5b7f632acadbe5153756db615c5d28bbc99f9bd0976d',
-            'account_ram_deltas': None,
-            'except': None,
-            'inline_traces': [{'receipt': {'receiver': 'dfuseiohooks',
-            'act_digest': '469d68d4e68c0adc6fd302ca60b4d27256400965b90baf7a5736c9e0ed0b3d0f',
-            'global_sequence': '6249291690',
-            'auth_sequence': [],
-            'recv_sequence': 2,
-            'code_sequence': 0,
-            'abi_sequence': 1},
-            'act': {'account': 'dfuseiohooks',
-            'name': 'event',
-            'data': {'data': 'testing=123', 'key': ''},
-            'hex_data': '000b74657374696e673d313233'},
-            'context_free': True,
-            'elapsed': 11,
-            'console': '',
-            'trx_id': '1d5f57e9392d045ef4d1d19e6976803f06741e11089855b94efcdb42a1a41253',
-            'block_num': 53280461,
-            'block_time': '2019-04-16T14:35:41.5',
-            'producer_block_id': '032cfecd63f2e42da2fb5b7f632acadbe5153756db615c5d28bbc99f9bd0976d',
-            'account_ram_deltas': None,
-            'except': None,
-            'inline_traces': None}]}],
-        'failed_dtrx_trace': None,
-        'except': None},
-        'execution_block_header': {'timestamp': '2019-04-16T14:35:41.5',
-        'producer': 'bitfinexeos1',
-        'confirmed': 0,
-        'previous': '032cfeccf3b57dd793d3ff831adb381225fa7c2b4a998a3c16502c2774581e36',
-        'transaction_mroot': 'a4317c9ce3c3925fac8bda2463a06d5b5876b37ea4b0b2af36bb42d028910a66',
-        'action_mroot': '16187708fc3827f31d2fbe8ecf6c1877e2b5f0050e63f025edd01eeff4dec82f',
-        'schedule_version': 773,
-        'new_producers': None,
-        'header_extensions': []},
-        'dtrxops': None,
-        'creation_tree': [],
-        'dbops': None,
-        'ramops': None,
-        'tableops': None,
-        'pub_keys': ['EOS8b3K9r6mLDykeL7GzGJCFt9Z5SRSHSXFT77yHz8e3z7en1tFwG'],
-        'created_by': None,
-        'canceled_by': None,
-        'execution_irreversible': True,
-        'creation_irreversible': True,
-        'cancelation_irreversible': False
-    }
-    """
-
     def __init__(self, id: str,
                  transaction_status: str, transaction: Transaction,
                  execution_trace: TransactionTrace, execution_block_header: BlockHeader = None,
@@ -908,3 +734,39 @@ class TransactionLifecycle:
         self.execution_irreversible = execution_irreversible
         self.creation_irreversible = creation_irreversible
         self.cancelation_irreversible = cancelation_irreversible
+
+
+class ABIType:
+    def __init__(self, **kwargs):
+        self.data = kwargs
+        self.block_num = kwargs.get('block_num')
+        self.account = kwargs.get('account')
+        self.abi = kwargs.get('abi')
+
+
+class Bin2JSONType:
+    def __init__(self, **kwargs):
+        self.data = kwargs
+        self.block_num = kwargs.get('block_num')
+        self.account = kwargs.get('account')
+        self.table = kwargs.get('table')
+        self.rows = kwargs.get('rows')
+
+
+class KeyAccountsType:
+    def __init__(self, **kwargs):
+        self.data = kwargs
+        self.block_num = kwargs.get('block_num')
+        self.account_names = kwargs.get('account_names')
+
+
+class PermissionLinkType:
+    def __init__(self, **kwargs):
+        self.data = kwargs
+        self.up_to_block_id = kwargs.get('up_to_block_id')
+        self.up_to_block_num = kwargs.get('up_to_block_num')
+        self.last_irreversible_block_id = kwargs.get(
+            'last_irreversible_block_id')
+        self.last_irreversible_block_num = kwargs.get(
+            'last_irreversible_block_num')
+        self.linked_permissions = kwargs.get('linked_permissions')
