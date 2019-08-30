e3ca094b3f822039837047f6ad24a7fd617fdb70
I'm The Cheese <ch33s3codes@gmail.com>
2019-08-30 17:01:20 +0300

    chore: update setup

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# To use a consistent encoding
+from os import path
+from codecs import open
+# Always prefer setuptools over distutils
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='dfuse-python',
+    packages=['dfuse-python'],
+    version='0.0.1',
+    description='Python wrapper around the dfuse.io API.',
+    author='Cheese',
+    author_email='ch33s3codes@gmail.com',
+    url='https://github.com/th3ch33s3/dfuse-python',
+    project_urls={
+        'Bug Reports': 'https://github.com/th3ch33s3/dfuse-python/issues',
+        'Buy me a coffee': 'https://github.com/th3ch33s3/dfuse-python#buy-me-a-coffee',
+    },
+    license='MIT License',
+    install_requires=[
+        'requests>=2.22.0',
+        'requests_cache>=0.5.2',
+        'websockets'='8.0.2',
+        'grpcio'="1.23.0,
+        'grpcio-tools'='1.23.0',
+        'python-decouple'='3.1',
+    ],
+    keywords=['EOSIO', 'API', 'dfuse'],
+    classifiers=[
+        'License :: OSI Approved :: MIT Software License',
+        'Development Status :: 1 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
