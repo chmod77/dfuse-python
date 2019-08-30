e8ac30085925f355f04c7d5856583a8da16fc648
I'm The Cheese <ch33s3codes@gmail.com>
2019-08-30 16:06:19 +0300

    chore: update details

--- dfuse/__init__.py
+++ dfuse/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__title__ = 'dfuespython'
+__title__ = 'dfuse'
 __version__ = '0.0.1'
 __author__ = '<ch33s3codes@gmail.com>'
 __repo__ = 'https://github.com/th3ch33s3/dfuse-python'
