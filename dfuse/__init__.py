b74c220a67f2bfde2ecd66abf6f7ab5d78ea3203
I'm The Cheese <ch33s3codes@gmail.com>
2019-09-11 20:45:04 +0300

    release: bump up version to 0.0.3

--- dfuse/__init__.py
+++ dfuse/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'dfuse'
-__version__ = '0.0.1'
+__version__ = '0.0.3'
 __author__ = '<ch33s3codes@gmail.com>'
 __repo__ = 'https://github.com/th3ch33s3/dfuse-python'
 __license__ = 'MIT License'
