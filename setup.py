0ba8487fa2d682e8a410e4ea70ea0130d1fe4248
chmodx <chmod777.ke@gmail.com>
2021-01-06 13:02:00 +0300

    chore: update setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from codecs import open
 # To use a consistent encoding
 from os import path
-from codecs import open
+
 from setuptools import find_packages, setup
 
 # Always prefer setuptools over distutils
