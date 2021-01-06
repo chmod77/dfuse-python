0f2d6694ea9769e8b59b3563733dc81ef8a4a8f0
chmodx <chmod777.ke@gmail.com>
2021-01-06 21:33:56 +0300

    chore: update tests

--- tests/test_get_block_at_timestamp.py
+++ tests/test_get_block_at_timestamp.py
@@ -1,10 +1,12 @@
-from dfuse.eosio import Eosio
 import datetime
+
+from dfuse.eosio import Eosio
 
 
 def test_get_block_at_timestamp():
     eosio = Eosio()
-    obj = eosio.get_block_at_timestamp(time=datetime.datetime.now()-datetime.timedelta(1), comparator='gte')
+    obj = eosio.get_block_at_timestamp(
+        time=datetime.datetime.now() - datetime.timedelta(1), comparator="gte"
+    )
     print(obj.data)
-    assert "block" is not obj.data
-    
+    assert "block" in obj.data
